d1fa0db62b4ac75bd0e3fb8b89e45e6158b49529
raphaelreynaud-web <contact.raphaelenlive@gmail.com>
2023-11-28 11:33:48 +0100

    Creation of the event card design

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Events;
+use Doctrine\ORM\EntityManagerInterface;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'events' => $entityManager->getRepository(Events::class)->findAll(),
         ]);
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -59,7 +59,7 @@ Encore
     .enablePostCssLoader()
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    /* .enableSassLoader() */
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
